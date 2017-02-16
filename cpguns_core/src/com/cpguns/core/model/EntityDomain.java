a98a8b5147222efd40f455ed3d0abcc377f03e17
Gustavo Bezerra <gustavo.bezerra@hotmail.com>
2017-02-16 15:09:42 -0200

    Adicionada a EntityDomain

---
+++ cpguns_core/src/com/cpguns/core/model/EntityDomain.java
@@ -0,0 +1,22 @@
+package com.cpguns.core.model;
+
+public class EntityDomain {
+    
+    private int id;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
