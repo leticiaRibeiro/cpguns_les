8d6f7a34de63ab06ba74432dc2032be318cf8927
Gustavo Bezerra <gustavo.bezerra@hotmail.com>
2017-03-15 10:56:55 -0300

    Menu da área logada do cliente jogado para uma diretiva, para reaproveitar em outras páginas

---
+++ cpguns_web/web/angular/directives/directive-menu-cliente.js
@@ -0,0 +1,11 @@
+angular.module('minhasDiretivas').directive('menuCliente', function(){
+	var ddo = {};
+
+	ddo.restrict = "AE";
+
+	ddo.templateUrl = "../angular/directives/menu_cliente.html";
+
+	return ddo;
+});
+
+
