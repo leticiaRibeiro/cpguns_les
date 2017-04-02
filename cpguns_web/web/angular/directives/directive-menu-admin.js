e60d5c1edd7ef0ac1a74905f69f78b152fa43482
Leticia Ribeiro <letribeiro_1@hotmail.com>
2017-04-02 16:46:14 -0300

    Home do Cliente e Admin. Diretiva do menu do Admin

---
+++ cpguns_web/web/angular/directives/directive-menu-admin.js
@@ -0,0 +1,11 @@
+angular.module('minhasDiretivas').directive('menuAdmin', function(){
+	var ddo = {};
+
+	ddo.restrict = "AE";
+
+	ddo.templateUrl = "../angular/directives/menu_admin.html";
+
+	return ddo;
+});
+
+
